ffd061c6600ead7701ac1e34f55c0f385e08656a
Grant Ryan <gtrfrost@gmail.com>
2025-05-18 21:29:08 +1000

    initial commit

---
+++ assets/scene/entity-names.ts
@@ -0,0 +1,14 @@
+// Auto-generated entity names from the scene
+
+
+/**
+ * Object containing all entity names in the scene for autocomplete support.
+ */
+export enum EntityNames {
+  aetheria_013001000_glb = "aetheria_013001000.glb",
+  aetheria_013001000_glb_2 = "aetheria_013001000.glb_2",
+  aetheria_013001000_x90_glb = "aetheria_013001000_x90.glb",
+  aetheria_013001000_z270_glb = "aetheria_013001000_z270.glb",
+  aetheria_345123001_x270_glb = "aetheria_345123001_x270.glb",
+  aetheria_555555333_y180_glb = "aetheria_555555333_y180.glb",
+} 
